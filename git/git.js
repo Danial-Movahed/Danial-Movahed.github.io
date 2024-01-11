21b579553dbcf7e4b59b4028d3bc217ada405cb5
Seyed Danial Movahed <movaheddanial@gmail.com>
2024-01-11 22:50:19 +0330

    Added checks for server config and added percentage

--- git/git.js
+++ git/git.js
@@ -1,29 +1,55 @@
-const SERVER_ADDR = "127.0.0.1";
-const SERVER_PORT = "5000";
+const SERVER_ADDR = Telegram.WebApp.CloudStorage.getItem("ServerAddress");
+const SERVER_PORT = Telegram.WebApp.CloudStorage.getItem("ServerPort");
 const socket = io("ws://" + SERVER_ADDR + ":" + SERVER_PORT);
 
 function clone() {
-    progress = document.getElementById("GitProgress");
-  
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://" + SERVER_ADDR + ":" + SERVER_PORT + "/git/clone", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        URL: "https://github.com/Danial-Movahed/SOAS",
-        Directory: "/home/danial/asghar"
-    }));
-  
-    progress.style.display = "inline";
-    progress.removeAttribute("value");
-    socket.on("CloneMaxProgress", (arg, callback) => {
-      progress.max = arg;
-      console.log(arg);
-    });
-    socket.on("CloneStatus", (arg, callback) => {
-      console.log(arg);
-    });
-    socket.on("CloneProgress", (arg, callback) => {
-      console.log(arg);
-      progress.value = arg;
-    });
+  if (SERVER_ADDR === "") {
+    Telegram.WebApp.showAlert("Please select a server!");
+    return;
   }
+  if (SERVER_PORT === "") {
+    Telegram.WebApp.showAlert("Please set the server port!");
+    return;
+  }
+  if (!socket.connected) {
+    Telegram.WebApp.showAlert("Something is wrong with the server connection, Please check your settings and try again!");
+    return;
+  }
+  ProgressBar = document.getElementById("GitProgressBar");
+  ProgressPercentage = document.getElementById("GitProgressPercentage");
+
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    "POST",
+    "http://" + SERVER_ADDR + ":" + SERVER_PORT + "/git/clone",
+    true
+  );
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(
+    JSON.stringify({
+      URL: document.getElementById("CloneURLInput").value,
+      Directory: document.getElementById("CloneDirInput").value,
+    })
+  );
+
+  ProgressBar.style.display = "inline";
+  ProgressPercentage.style.display = "inline";
+  ProgressBar.removeAttribute("value");
+  socket.on("CloneMaxProgress", (arg, callback) => {
+    ProgressBar.max = arg;
+  });
+  socket.on("CloneStatus", (arg, callback) => {
+    ProgressBar.style.display = "none";
+    ProgressPercentage.style.display = "none";
+    console.log(arg);
+    if (arg === "Success") {
+      return;
+    }
+    Telegram.WebApp.showAlert("Error occurred while cloning the repository: " + arg);
+  });
+  socket.on("CloneProgress", (arg, callback) => {
+    ProgressBar.value = arg;
+    ProgressPercentage.innerHTML =
+      String(Math.round((arg / ProgressBar.max) * 100 * 100) / 100) + "%";
+  });
+}
