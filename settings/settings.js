3e80de35b58493c43fcc2ec72fdc5b790b261da9
Seyed Danial Movahed <movaheddanial@gmail.com>
2024-01-12 06:25:32 +0330

    Changed test server

--- settings/settings.js
+++ settings/settings.js
@@ -1,4 +1,4 @@
 function setupTest() {
     Telegram.WebApp.CloudStorage.setItem("ServerAddress", "31.25.135.171");
-    Telegram.WebApp.CloudStorage.setItem("ServerPort", "5000");
+    Telegram.WebApp.CloudStorage.setItem("ServerPort", "443");
 }
